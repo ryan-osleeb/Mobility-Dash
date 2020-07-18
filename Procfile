50bffd65f8759c45c66270e426dc133cb838a290
Ryan Osleeb <rosleeb@college.harvard.edu>
2020-07-18 12:25:20 -0400

    worker test

--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
 web: gunicorn mobility_dash:server
 worker: python apple_maps_dash.py
-worker: python google_mobility.py
-worker: ny_thruway.py
--- mobility_dash.py
+++ mobility_dash.py
@@ -8,8 +8,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from apple_maps_dash import am_us, am_risk, am_heat
-from google_mobility import us_fig, parks_fig, workplace_fig
-from ny_thruway import ny_cars_rolling
+# from google_mobility import us_fig, parks_fig, workplace_fig
+# from ny_thruway import ny_cars_rolling
 
 
 app = dash.Dash()
@@ -20,31 +20,31 @@ app.layout = html.Div(children=[html.H1(children=''),
                                 id = 'US Driving',
                                 figure=am_us
                             ),
-                        html.H2(children=''),
-                        dcc.Graph(
-                                id = 'US Heat Map',
-                                figure=am_heat
-                            ),
-                        html.H3(children=''),
-                        dcc.Graph(
-                                id = 'US Google Mobility',
-                                figure=us_fig
-                            ),
-                        html.H4(children=''),
-                        dcc.Graph(
-                                id = 'Parks Mobility Heat Map',
-                                figure=parks_fig
-                            ),
-                        html.H5(children=''),
-                        dcc.Graph(
-                                id = 'Workplace Mobility Heat Map',
-                                figure=workplace_fig
-                            ),
-                        html.H6(children=''),
-                        dcc.Graph(
-                                id = 'NY Thruway',
-                                figure=ny_cars_rolling
-                            )
+                        # html.H2(children=''),
+                        # dcc.Graph(
+                        #         id = 'US Heat Map',
+                        #         figure=am_heat
+                        #     ),
+                        # html.H3(children=''),
+                        # dcc.Graph(
+                        #         id = 'US Google Mobility',
+                        #         figure=us_fig
+                        #     ),
+                        # html.H4(children=''),
+                        # dcc.Graph(
+                        #         id = 'Parks Mobility Heat Map',
+                        #         figure=parks_fig
+                        #     ),
+                        # html.H5(children=''),
+                        # dcc.Graph(
+                        #         id = 'Workplace Mobility Heat Map',
+                        #         figure=workplace_fig
+                        #     ),
+                        # html.H6(children=''),
+                        # dcc.Graph(
+                        #         id = 'NY Thruway',
+                        #         figure=ny_cars_rolling
+                        #     )
                             ])
 
 if __name__ == '__main__':
